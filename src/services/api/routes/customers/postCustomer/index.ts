ff400796669092daab5b1a6402c988cf63f7665d
thiagozampieri <thiagoracyz@gmail.com>
2023-11-07 15:41:01 -0300

    feat: implementa rota de criar cliente

--- src/services/api/routes/customers/postCustomer/index.ts
+++ src/services/api/routes/customers/postCustomer/index.ts
@@ -1,10 +1,41 @@
-import { useLocalStorage } from '@hooks/useLocalStorage'
-import { HttpPostCustomerPayload } from './types'
 import { makeError } from '@utils/functions/makeError'
+import { ICustomer } from '@services/types/Customer'
+import { HttpPostCustomerPayload } from './types'
+import { useLocalStorage } from '@hooks/useLocalStorage'
 
 // TODO: Implementar lógica
 export async function postCustomer(
   payload?: HttpPostCustomerPayload
 ): Promise<void> {
+  const requiredFields = ['name', 'email', 'phone', 'document']
+
+  const { name, email, document , phone, } = payload
+
+  requiredFields.forEach(param => {
+    if (!payload[param])
+      throw makeError(
+        'Missing Param',
+        `O parametro "${param}" está faltando`,
+        '400'
+      )
+  })
+
+  let customers: ICustomer[] = useLocalStorage().getItem("customers")
+
+  if ( !customers ) {
+    customers = []
+  }
+  const customer: ICustomer = {
+    id: crypto.randomUUID(),
+    name,
+    email,
+    document,
+    phone
+  }
+
+  customers.push(customer)
+
+  useLocalStorage().saveItem("customers", customers)
+
   return null
 }
