75ca0c7b2d2f1aa985207140ca537fafe0100460
Guilherme <guilhermelopes035@gmail.com>
2023-11-07 00:13:52 -0300

    feat: implementa rota de post pet

--- src/services/api/routes/pets/postPet/index.ts
+++ src/services/api/routes/pets/postPet/index.ts
@@ -1,6 +1,35 @@
+import { IPet } from '@services/types/Pet'
 import { HttpPostPetPayload } from './types'
+import { useLocalStorage } from '@hooks/useLocalStorage'
+import { makeError } from '@utils/functions/makeError'
 
-// TODO: Implementar lógica
 export async function postPet(payload?: HttpPostPetPayload): Promise<void> {
-  return null
+  const requiredFields = ['name', 'type', 'breed', 'customerId', 'customerName']
+
+  const { name, type, breed, customerId, customerName } = payload
+  requiredFields.forEach(param => {
+    if (!payload[param])
+      throw makeError(
+        'Missing Param',
+        `O parametro ${param} está faltando`,
+        '400'
+      )
+  })
+
+  const pet: IPet = {
+    id: crypto.randomUUID(),
+    name,
+    type,
+    breed,
+    customerId,
+    customerName
+  }
+
+  const { getItem, saveItem } = useLocalStorage()
+
+  const pets = getItem('pets')
+
+  pets.push(pet)
+
+  saveItem('pets', pets)
 }
