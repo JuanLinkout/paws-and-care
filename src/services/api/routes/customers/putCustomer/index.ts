d037d6a21c357707290e0bd3018b7d675efac9dc
thiagozampieri <thiagoracyz@gmail.com>
2023-11-07 18:07:58 -0300

    feat: adiciona rota para editar clientes

--- src/services/api/routes/customers/putCustomer/index.ts
+++ src/services/api/routes/customers/putCustomer/index.ts
@@ -1,9 +1,30 @@
+import { useLocalStorage } from '@hooks/useLocalStorage'
 import { HttpPutCustomerPayload } from './types'
+import { ICustomer } from '@services/types/Customer'
 
 // TODO: Implementar lógica
 export async function putCustomer(
   id: string,
   payload?: HttpPutCustomerPayload
 ): Promise<void> {
+  const { name, email, document , phone, } = payload
+
+  let customers: ICustomer[] = useLocalStorage().getItem("customers")
+
+  const editedCustomers: ICustomer[] = customers.map(item =>  { 
+    if (item.id === id ){
+    item = {
+      id: item.id,
+      name,
+      email,
+      document,
+      phone
+    }
+  }
+  return item
+  })
+  
+  useLocalStorage().saveItem("customers", editedCustomers)
+
   return null
 }
