05b3e8126d68c20b2302e63b0e413246f4064d86
Guilherme <guilhermelopes035@gmail.com>
2023-11-07 00:14:20 -0300

    feat: implementa rota de put pets

--- src/services/api/routes/pets/putPet/index.ts
+++ src/services/api/routes/pets/putPet/index.ts
@@ -1,9 +1,23 @@
+import { useLocalStorage } from '@hooks/useLocalStorage'
 import { HttpPutPetPayload } from './types'
+import { HttpGetPetsResponse } from '../getPets/types'
+import { makeError } from '@utils/functions/makeError'
 
-// TODO: Implementar lógica
 export async function putPet(
   id: string,
   payload?: HttpPutPetPayload
 ): Promise<void> {
-  return null
+  const { getItem, saveItem } = useLocalStorage()
+
+  const pets: HttpGetPetsResponse = getItem('pets')
+
+  const petIndex = pets.findIndex(pet => pet.id === id)
+
+  if (petIndex === -1) {
+    throw makeError('Not Found', 'Pet não encontrado', '404')
+  }
+
+  pets[petIndex] = { ...pets[petIndex], ...payload }
+
+  saveItem('pets', pets)
 }
