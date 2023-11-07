aefa2340b2efc36a2f0fb290af9c0006ae5cd4bc
thiagozampieri <thiagoracyz@gmail.com>
2023-11-07 15:49:03 -0300

    feat: implementa rota de deletar cliente

--- src/services/api/routes/customers/deleteCustomer/index.ts
+++ src/services/api/routes/customers/deleteCustomer/index.ts
@@ -1,4 +1,11 @@
+import { useLocalStorage } from "@hooks/useLocalStorage";
+
 // TODO: Implementar lógica
 export async function deleteCustomer(id: string): Promise<void> {
-  return null
+
+  const customers = useLocalStorage().getItem("customers")
+
+  const notDeletedCustomers = customers.filter( item => item.id !== id)
+
+  useLocalStorage().saveItem("customers", notDeletedCustomers)
 }
